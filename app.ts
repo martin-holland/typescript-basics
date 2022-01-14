2e673f8f7aa11f374e93a40775886f0767b485ee
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-01-14 14:42:07 +0200

    literals

--- app.ts
+++ app.ts
@@ -1,15 +1,24 @@
-function combine(input1: number | string, input2: number | string)  { //union type
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text')  { //union type this can prevent any other value.
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-        return result;
-    }
+    return result
+    // the above code is better because it allows the casting if one parameter is missing.
+    // if (resultConversion === 'as-number') {
+    //     return +result;
+    // } else {
+    //     return result.toString(); 
+    // }
+}
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges)
 
-const combinedNames = combine('Max', 'Anna');
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges)
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames)
