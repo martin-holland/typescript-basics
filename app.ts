3b01a2a513b1b013463e1f50c289f776aa2cb24a
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-01-16 12:51:40 +0200

    return types and voids

--- app.ts
+++ app.ts
@@ -1,27 +1,11 @@
-type Combinable = number | string; // declaring the union type in advance to be reused.
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor)  { //union type this can prevent any other value.
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result
-    // the above code is better because it allows the casting if one parameter is missing.
-    // if (resultConversion === 'as-number') {
-    //     return +result;
-    // } else {
-    //     return result.toString(); 
-    // }
+function add(n1: number, n2:number): number {
+    return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges)
+function printResult(num: number): void { // this function doesnt have a return statement. If you set the type as undefinied, Typescript would expect you to return, but return nothing.
+    console.log('Result: ' + num);
+}
 
-const combinedStringAges = combine('30', '26', 'as-text');
-console.log(combinedStringAges)
+printResult(add(5,12))
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames)
+// let someValue: undefinied,
